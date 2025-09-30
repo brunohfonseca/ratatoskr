3adec6eb55d2f4e3873c132136ec858ef97c312f
Bruno Honorato da Fonseca <bruno.fonseca@ixcsoft.com.br>
2025-09-30 08:06:29 -0300

    feat: add triggers to update 'updated_at' column for users and alert_groups

---
+++ migrations/20250930110418_updadet_at_triggers.up.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE FUNCTION update_updated_at_column()
+    RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = now();
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_users_updated_at BEFORE UPDATE ON users
+    FOR EACH ROW EXECUTE FUNCTION update_updated_at_column();
+
+CREATE TRIGGER update_alert_groups_updated_at BEFORE UPDATE ON alert_groups
+    FOR EACH ROW EXECUTE FUNCTION update_updated_at_column();
